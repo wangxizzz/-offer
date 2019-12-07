b315f2056648a9401420f17cb1e5caeeea876e64
wangxizzz <979675820@qq.com>
2019-12-07 21:20:37 +0800

    Set存储List问题

---
+++ 2019/src/基础算法/day01/子集问题/TestSet.java
@@ -0,0 +1,26 @@
+package 基础算法.day01.子集问题;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author wangxi
+ * @Time 2019/12/7 18:06
+ * AbstractList重写了hashCode与equals()方法。因此可以使用Set存储
+ */
+public class TestSet {
+    public static void main(String[] args) {
+        Set<List<Integer>> result = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        result.add(list);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(2);
+        System.out.println(result.add(list2));
+    }
+}
