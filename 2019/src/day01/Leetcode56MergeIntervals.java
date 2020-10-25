04389f118efb4334bb012b7d35fe86ba4ce97ef7
wangxizzz <979675820@qq.com>
2020-10-25 18:07:06 +0800

    区间合并

--- 2019/src/day01/Leetcode56MergeIntervals.java
+++ 2019/src/day01/Leetcode56MergeIntervals.java
@@ -15,31 +15,36 @@ public class Leetcode56MergeIntervals {
         if (intervals == null || intervals.length <= 0) {
             return new int[0][0];
         }
-        List<int[]> result = new ArrayList<>();
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return Integer.compare(o1[0], o2[0]);
             }
         });
+        List<int[]> res = new ArrayList<>();
         int j = 0;
         for (int i = 0; i < intervals.length; i++) {
             if (i == 0) {
-                result.add(intervals[0]);
+                res.add(intervals[i]);
                 j++;
                 continue;
             }
-            int[] pre = result.get(j - 1);
-            if (pre[1] >= intervals[i][0] && pre[1] < intervals[i][1]) {
-                pre[1] = intervals[i][1];
-                result.set(j - 1, pre);
-            } else if (pre[1] < intervals[i][0]) {
-                result.add(intervals[i]);
+            int[] left = res.get(j - 1);
+            int[] right = intervals[i];
+            if (left[1] >= right[0] && left[1] <= right[1]) {
+                left[1] = right[1];
+            } else if (left[1] >= right[0] && left[1] >= right[1]) {
+                continue;
+            } else {
+                res.add(intervals[i]);
                 j++;
             }
         }
-        int[][] nums = new int[result.size()][];
-        return result.toArray(nums);
+        int[][] resArr = new int[res.size()][];
+        for (int i = 0; i < res.size(); i++) {
+            resArr[i] = res.get(i);
+        }
+        return resArr;
     }
 }
 
