75b7ff1387ad8eb0fc5184409ac5ccc12ed57b42
wangxi03 <wangxi03@xdfzx.com>
2021-03-01 13:16:20 +0800

    最大字段和的 窗口下标暂存

--- 2019/src/leecode/面试题/MaxSubArray.java
+++ 2019/src/leecode/面试题/MaxSubArray.java
@@ -1,5 +1,8 @@
 package leecode.面试题;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author wangxi
  * @Time 2020/5/1 00:20
@@ -15,6 +18,8 @@ public class MaxSubArray {
         int maxSum = Integer.MIN_VALUE;
         int p = 0;
         int q = 0;
+        List<String> list = new ArrayList<>();
+        String ss = "";
         for (int i = 0; i < nums.length; i++) {
             if (i == 0 || curSum <= 0) {
                 curSum = nums[i];
@@ -27,14 +32,20 @@ public class MaxSubArray {
                 maxSum = curSum;
                 // end指针在哪里切换的
                 q = i;
+
+                list.add(p + "_" + q);
+                // 利用其它容器保存 窗口的两个边界，不让后续数字破坏窗口边界值
+                ss = p + "_" + q;
             }
         }
         System.out.println("起始下标: " + p + " 结束下标: " + q);
+        System.out.println(list);
+        System.out.println(ss);
         return maxSum;
     }
 
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-50,4, 100};
+        int[] nums = {-2,1,-3,10000,4,-100000,2,1,-50,4, 100};
         System.out.println(new MaxSubArray().maxSubArray(nums));
     }
 }
